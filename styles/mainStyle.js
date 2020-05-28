cf850b4cab9c188190378ad9cbf6861d89f606e7
ku <rnwkfydwkd82@likelion.org>
2020-05-28 15:32:54 +0900

    first commit

---
+++ styles/mainStyle.js
@@ -0,0 +1,91 @@
+const mainStyle = {
+  menu: {
+    width: 40,
+    height: 40,
+  },
+  logoTxt: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  headerInner: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+  container: {
+    flex: 1,
+  },
+  header: {
+    height: 70,
+    backgroundColor: 'white',
+    marginBottom: 10,
+  },
+  marginBottom: {
+    marginBottom: 30,
+  },
+  body: {
+    flex: 1,
+    backgroundColor: '#F3EDE6',
+    paddingTop: 50,
+  },
+  innerBodyOne: {
+    paddingTop: 30,
+    flex: 2,
+    paddingLeft: 30,
+  },
+  innerBodyTwo: {
+    flex: 3,
+    backgroundColor: 'white',
+    paddingLeft: 30,
+  },
+  arrImg: {
+    width: 150,
+    height: 20,
+    position: 'absolute',
+    right: 50,
+    bottom: 100,
+  },
+  innerTxtBig: {
+    fontWeight: 'bold',
+    fontSize: 40,
+    top: -50,
+    lineHeight: 50,
+  },
+  innerTxtSmall: {
+    position: 'absolute',
+    right: 0,
+    fontSize: 20,
+    textAlign: 'left',
+    paddingRight: 30,
+    lineHeight: 30,
+    top: 150,
+  },
+  bodyInnerTxt: {
+    fontSize: 30,
+    marginBottom: 20,
+  },
+  impaTxt: {
+    width: 120,
+    backgroundColor: '#F1D5CA',
+    paddingLeft: 10,
+    fontWeight: 'bold',
+  },
+  fontBold: {
+    fontWeight: 'bold',
+  },
+  bottomImg: {
+    height: 60,
+    width: 5,
+  },
+  bottomImgCon: {
+    position: 'absolute',
+    flex: 1,
+    bottom: 50,
+    left: '50%',
+  },
+};
+
+export {mainStyle};
